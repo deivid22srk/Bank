b9a1d82d7fc74fbb5b2a5ce39255f3e0e95a9181
deivid22srk <psvstore01@gmail.com>
2025-11-28 18:58:50 -0300

    Recriado automaticamente em 2025-11-28 18:58:50

--- app/src/main/java/com/bancoapp/security/NetworkSecurity.kt
+++ app/src/main/java/com/bancoapp/security/NetworkSecurity.kt
@@ -1,6 +1,7 @@
 package com.bancoapp.security
 
 import android.util.Log
+import com.bancoapp.BuildConfig
 import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
